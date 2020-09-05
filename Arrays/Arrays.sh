9cd2814f2e4997186657a78c3486852b92694c11
Sobhit Sharma <sobhit.sharma@kaleyra.com>
2020-09-05 12:11:17 +0530

    Added array basics

---
+++ Arrays/Arrays.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+my_array=(apple banana "Fruit Basket" orange)
+
+echo ${my_array[3]}                     # orange - note that curly brackets are needed
+
+# adding another array element
+my_array[4]="carrot"                    # value assignment without a $ and curly brackets
+
+echo ${#my_array[@]}                    # 5
+
+echo ${my_array[${#my_array[@]}-1]}
